1eb604d198d0c56efdbbad2769ab5287ebd80fad
Adi <adhiraj.sirohi@gmail.com>
2024-04-08 13:51:25 +0530

    Image Comparison Slider
    Slider to show 2 images side-by-side + sliderLine to drag and view the images + related js & CSS + Image assets

---
+++ js/brutalish-image-comparison.js
@@ -0,0 +1,22 @@
+/*
+Project image comparison slider
+*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    const slider = document.querySelector(".project-image-comparison-slider");
+    const beforeImage = document.querySelector(".before-image");
+    const sliderLine = document.querySelector(".project-image-comparison-slider-line");
+
+    slider.addEventListener("input", function() {
+        let sliderValue = this.value;
+        let percentage = sliderValue + "%";
+
+        /*
+        As the sliderValue changes, the width of the beforeImage is changed accordingly
+        and so is the 'left' position of the project-image-comparison-slider-line
+        By default the project-image-comparison-slider-line is left: 50%; as defined in the CSS
+         */
+        beforeImage.style.width = percentage;
+        sliderLine.style.left = percentage;
+    });
+});
